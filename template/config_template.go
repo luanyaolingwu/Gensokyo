db685f709cbab98fd423e45584ec25ec2e5629a8
鸞瑤綾舞 <105547626+luanyaolingwu@users.noreply.github.com>
2024-04-25 09:32:56 +0800

    NONE

--- template/config_template.go
+++ template/config_template.go
@@ -219,15 +219,15 @@ settings:
 
 `
 const Logo = `
-'                                                                                                      
-'    ,hakurei,                                                      ka                                  
-'   ho"'     iki                                                    gu                                  
-'  ra'                                                              ya                                  
-'  is              ,kochiya,    ,sanae,    ,Remilia,   ,Scarlet,    fl   and  yu        ya   ,Flandre,   
-'  an      Reimu  'Dai   sei  yas     aka  Rei    sen  Ten     shi  re  sca    yu      ku'  ta"     "ko  
-'  Jun        ko  Kirisame""  ka       na    Izayoi,   sa       ig  Koishi       ko   mo'   ta       ga  
-'   you.     rei  sui   riya  ko       hi  Ina    baI  'ran   you   ka  rlet      komei'    "ra,   ,sa"  
-'     "Marisa"      Suwako    ji       na   "Sakuya"'   "Cirno"'    bu     sen     yu''        Satori  
-'                                                                                ka'                   
-'                                                                               ri'                    
+'                                                                                                           
+'    ,hakurei,                                                         ka                                   
+'   ho"'     iki                                                       gu                                   
+'  ra'                                                                 ya                                   
+'  is                ,kochiya,   52|,sanae,    ,Remilia,   ,Scarlet,   fl   and  yu        ya   ,Flandre,   
+'  an       Reimu   Dai_____sei  yas     'ka  Rei    sen  Ten     shi  re  sca    yu      ku'  ta"     "ko  
+'  Jun         ko   8PP""""""""  na       yu    "Mirai,   sa       ig  Koishi       ko   mo'   ta       ga  
+'   you.     rei    sui     ,aa  ko       hi  Ina    baI  'ran   you   ka  rlet      komei'    "ra,   ,sa"  
+'     "Marisa"       'LunaRyu'   ji       na   "Sakuya"'    "Cirno"'   bu     sen     yu''       'Satori    
+'                                                                                    ka'                    
+'                                                                                   ri'                                        
 `
